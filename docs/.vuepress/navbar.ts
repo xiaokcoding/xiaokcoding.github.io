70f3bfd49ea9bb28d41f18cdd102c5809154533f
xiaokcoding <1512392199@qq.com>
2025-04-05 21:42:13 +0800

    test notes1

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -7,7 +7,7 @@ export const zhNavbar = defineNavbarConfig([
   { text: '归档', link: '/blog/archives/' },
   {
     text: '笔记',
-    items: [{ text: '示例', link: '/notes/demo/README.md' }]
+    items: [{ text: 'C++', link: '/notes/C++/README.md' }]
   },
 ])
 
