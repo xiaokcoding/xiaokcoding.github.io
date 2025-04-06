99f5313e025be7f75f35b46bf48a33449eaf0c26
xiaokcoding <1512392199@qq.com>
2025-04-06 14:30:47 +0800

    test

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -3,11 +3,11 @@ import { defineNavbarConfig } from 'vuepress-theme-plume'
 export const zhNavbar = defineNavbarConfig([
   { text: '首页', link: '/' },
   { text: '博客', link: '/blog/' },
-  { text: '标签', link: '/blog/tags/' },
-  { text: '归档', link: '/blog/archives/' },
+  //{ text: '标签', link: '/blog/tags/' },
+ // { text: '归档', link: '/blog/archives/' },
   {
     text: '笔记',
-    items: [{ text: 'Cpp', link: '/notes/Cpp/README.md' }]
+    items: [{ text: 'Cpp', link: '/notes/Cpp/' }]
   },
 ])
 
