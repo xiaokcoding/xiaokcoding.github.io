66627452a669b3bf1f943c7049eb6cb3bc5fb095
xiaokcoding <1512392199@qq.com>
2025-04-06 15:29:02 +0800

    test11

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -4,10 +4,14 @@ export const zhNavbar = defineNavbarConfig([
   { text: '首页', link: '/' },
   { text: '博客', link: '/blog/' },
   //{ text: '标签', link: '/blog/tags/' },
- // { text: '归档', link: '/blog/archives/' },
+  //{ text: '归档', link: '/blog/archives/' },
   {
-    text: '笔记',
-    items: [{ text: 'Cpp', link: '/notes/Cpp/' }]
+    text: '系列文章',
+    items: [{ text: 'Cpp', link: '/Cpp/', icon: 'simple-icons:cplusplus' }]
+  },
+  {
+    text: '更多',
+    items: [{ text: ' ', link: '/' }]
   },
 ])
 
--- docs/.vuepress/notes.ts
+++ docs/.vuepress/notes.ts
@@ -3,11 +3,11 @@ import { defineNoteConfig, defineNotesConfig } from 'vuepress-theme-plume'
 const Cpp = defineNoteConfig({
   dir: 'Cpp',
   link: '/Cpp/',
-  sidebar: ['auto'],
+  sidebar: 'auto'
 })
 
 export const zhNotes = defineNotesConfig({
   dir: 'notes',
   link: '/',
-  notes: [Cpp],
+  notes: [Cpp]
 })
