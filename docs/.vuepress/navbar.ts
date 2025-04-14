d2323855bd3a1fa074b2222c214ba0b45f641640
xiaokcoding <1512392199@qq.com>
2025-04-14 21:54:15 +0800

    theme

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -1,47 +1,29 @@
 import { defineNavbarConfig } from 'vuepress-theme-plume'
 
 export const zhNavbar = defineNavbarConfig([
+  { text: '🏠首页', link: '/' },
+  { text: '📑博客', link: '/blog/' },
+  //{ text: '🏷️标签', link: '/blog/tags/' },
+  //{ text: '🗄️归档', link: '/blog/archives/' },
   {
-    text: '首页',
-    link: '/',
-    icon: 'material-symbols:home-rounded'
-  },
-  {
-    text: '博客',
-    link: '/blog/',
-    icon: 'mdi:book-open'
-  },
-  {
-    text: '系列文章',
+    text: '🗂️系列文章',
     items: [
-      {
-        text: 'Cpp',
-        link: '/Cpp/',
-        icon: 'mdi:language-cpp'
-      },
-      {
-        text: 'algorithms',
-        link: '/algorithms/',
-        icon: 'tabler:binary-tree'
-      },
-      {
-        text: 'Linux',
-        link: '/Liunx/',
-        icon: 'mdi:linux'
-      },
-      {
-        text: 'network',
-        link: '/network/',
-        icon: 'mdi:network'
-      },
+      { text: 'Cpp', link: '/Cpp/', icon: 'vscode-icons:file-type-cpp3' },
+      { text: 'algorithms', link: '/algorithms/', icon: 'tabler:binary-tree' },
+      { text: 'Linux', link: '/Liunx/', icon: 'devicon:linux' },
+      { text: 'network', link: '/network/', icon: 'material-icon-theme:gemini' },
     ]
   },
   {
     text: '更多',
-    items: [{
-      text: '关于我',
-      link: '/',
-      icon: 'material-symbols:more-horiz'
-    }]
+    icon: 'mingcute:more-3-fill',
+    items: [
+      {
+        text: '站点导航',
+        link: '/sites-collect/',
+        icon: 'mdi:roadmap',
+        activeMatch: '^/sites-collect',
+      },
+    ]
   },
 ])
